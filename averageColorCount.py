f0a7f3b5fc45cf0c87b0db4ca55e102521490e9c
li4mm4rtin <43616561+li4mm4rtin@users.noreply.github.com>
2022-07-20 13:24:05 -0400

    Add files via upload

--- averageColorCount.py
+++ averageColorCount.py
@@ -2,25 +2,61 @@ import cv2
 import numpy as np
 import sys
 from tkinter import filedialog
-import csv
 import os
+import copy
 
 np.set_printoptions(threshold=sys.maxsize)
 
-imageFile = filedialog.askopenfilename(title='Select Image File')
 
+def findRegionAverage(img, cStart, colWidthValue, rStart, rowHeightValue):
+    color = np.full(3, 0)
+    count = 0
+    for xPosition in range(int(colWidthValue)):
+        for yPosition in range(int(rowHeightValue)):
+            color += img[int(rStart + yPosition), int(cStart + xPosition), :]
+            count += 1
+
+    if count == 0:
+        count = 1
+
+    return color * 1/count
+
+
+def getMousePoints(event, x, y, flags, params):
+    global points
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points = x, y
+
+
+imageFile = filedialog.askopenfilename(title='Select Image File')
 image = cv2.imread(imageFile)
 
-cols = 0
-rows = 0
 width = 0
 colWidth = np.zeros(0)
 height = 0
 rowHeight = np.zeros(0)
 
+run = True
+
+points = (0, 0)
+cache = copy.deepcopy(image)
+
+while run:
+    cv2.setMouseCallback("Loaded Image", getMousePoints)
+    cv2.imshow("Loaded Image", cache)
+
+    if points != (0, 0):
+        cv2.line(cache, (points[0], 0), (points[0], image.shape[0]), (255, 0, 0), 1)
+        cv2.line(cache, (0, points[1]), (image.shape[1], points[1]), (255, 0, 0), 1)
+
+    if cv2.waitKey(33) == ord(' '):
+        break
+
+    cv2.imshow("Loaded Image", image)
+
 for i in range(image.shape[1]):
-    if np.all(np.isin(image[5, i, :], [0, 0, 0])):
-        cols += 1
+    if np.all(np.isin(image[points[1], i, :], [0, 0, 0])):
         colWidth = np.append(colWidth, width)
         width = -1
     elif i == image.shape[1] - 1:
@@ -29,8 +65,7 @@ for i in range(image.shape[1]):
     width += 1
 
 for i in range(image.shape[0]):
-    if np.all(np.isin(image[i, 5, :], [0, 0, 0])):
-        rows += 1
+    if np.all(np.isin(image[i, points[0], :], [0, 0, 0])):
         rowHeight = np.append(rowHeight, height)
         height = -1
     elif i == image.shape[0] - 1:
@@ -43,22 +78,7 @@ if rowHeight[0] == 0:
 if colWidth[0] == 0:
     colWidth = colWidth[1:]
 
-
-def findRegionAverage(img, cStart, colWidthValue, rStart, rowHeightValue):
-    color = np.full(3, 0)
-    count = 0
-    for xPosition in range(int(colWidthValue)):
-        for yPosition in range(int(rowHeightValue)):
-            color += img[int(rStart + yPosition), int(cStart + xPosition), :]
-            # print(img[cStart + xPosition, rStart + yPosition, :][0], img[cStart + xPosition, rStart + yPosition, :][1], img[cStart + xPosition, rStart + yPosition, :][2])
-            count += 1
-        # print(' ')
-
-    if count == 0:
-        count = 1
-
-    return color * 1/count
-
+print(colWidth, rowHeight)
 
 colStart = 0
 
@@ -72,8 +92,12 @@ for i in range(len(colWidth)):
     colStart = colStart + colWidth[i] + 1
 
 
-cv2.imwrite(os.path.join(os.path.dirname(imageFile), os.path.basename(imageFile)[:-5]+'_averagePixel.png'), averageColor)
-# np.savetxt(os.path.join(os.path.dirname(imageFile), os.path.basename(imageFile)[:-5]+"_averageBlueData.csv"), averageColor[:, :, 0], delimiter=",")
-# np.savetxt(os.path.join(os.path.dirname(imageFile), os.path.basename(imageFile)[:-5]+"_averageGreenData.csv"), averageColor[:, :, 1], delimiter=",")
-# np.savetxt(os.path.join(os.path.dirname(imageFile), os.path.basename(imageFile)[:-5]+"_averageRedData.csv"), averageColor[:, :, 2], delimiter=",")
+cv2.imwrite(os.path.join(os.path.dirname(imageFile), os.path.basename(imageFile)[:-5]+'_averagePixel.png'),
+            averageColor)
+# np.savetxt(os.path.join(os.path.dirname(imageFile), os.path.basename(imageFile)[:-5]+"_averageBlueData.csv"),
+           # averageColor[:, :, 0], delimiter=",")
+# np.savetxt(os.path.join(os.path.dirname(imageFile), os.path.basename(imageFile)[:-5]+"_averageGreenData.csv"),
+           # averageColor[:, :, 1], delimiter=",")
+# np.savetxt(os.path.join(os.path.dirname(imageFile), os.path.basename(imageFile)[:-5]+"_averageRedData.csv"),
+           # averageColor[:, :, 2], delimiter=",")
 
